17ed18b16d4536a62ef764823502b88874104b12
Park June Chul <pjc0247@naver.com>
2014-05-09 21:03:41 +0900

    Update c.js

--- c.js
+++ c.js
@@ -8,7 +8,9 @@ art.src = "album.png";
 getColorList = function(pixels, w,h){
   var colorList = new Array();
   var map = new Array();
+  var candidates = new Array();
   
+  /* 빈도맵을 만든다 */
   for(i=0;i<w;i++){
     for(j=0;j<h;j++){
       idx = (j * w * 4 + i * 4);
@@ -18,7 +20,28 @@ getColorList = function(pixels, w,h){
     }
   }
   
-  return colorList;
+  for(i=0;i<5;i++){
+    var max = {
+     key : "", /* 색상 키 */
+      hit : 0 /* 발생 빈도 */
+    }; 
+    
+    for(k in map){
+      if( map[k] > max.hit ){
+        var flag = true;
+        for(c in candidates){
+          if( c == k )
+            flag = false;
+        }
+      
+        if( flag == true){
+          candidates.push( map[k] );
+        }
+      }
+    }
+  }
+  
+  return candidates;
 }
 art.onload = function(){
   dc.drawImage( art, 0,0, 32,32 );
@@ -26,4 +49,7 @@ art.onload = function(){
   var pixels = dc.getImageData( 0,0,32,32 );
   
   var colorList = getColorList( pixels, 32,32 );
+  
+  console.log( colorList );
+  dc.fillStyle = "rgb(" + colorList[0] + ");";
 }
