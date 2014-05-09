a5964cd60c72d710403871bea895bfe3e6769ee5
Park June Chul <pjc0247@naver.com>
2014-05-09 12:44:13 +0900

    Update c.js

--- c.js
+++ c.js
@@ -3,6 +3,7 @@ var canvas = document.getElementById("canvas");
 var dc = canvas.getContext("2d");
 
 console.log( dc );
+console.log( "QWER ");
 
 for(i=0;i<10;i++){
   dc.fillStyle = "rgb(" + i*25 + ",0,0)";
