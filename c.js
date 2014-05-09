1330a40f2f231c4cd14c32f43a66d6f0c9f3e273
Park June Chul <pjc0247@naver.com>
2014-05-09 12:46:34 +0900

    Update c.js

--- c.js
+++ c.js
@@ -9,3 +9,11 @@ for(i=0;i<10;i++){
   dc.fillStyle = "rgb(" + i*25 + ",0,0)";
   dc.fillRect(0+30*i,0, 30,30);
 }
+for(i=0;i<10;i++){
+  dc.fillStyle = "rgb(0," + i*25 + ",0)";
+  dc.fillRect(0+30*i,30, 30,30);
+}
+for(i=0;i<10;i++){
+  dc.fillStyle = "rgb(0,0," + i*25 + ")";
+  dc.fillRect(0+30*i,60, 30,30);
+}
