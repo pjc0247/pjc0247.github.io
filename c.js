9a90b239137aedf10458c3687730650bcc8690e1
Park June Chul <pjc0247@naver.com>
2014-05-09 12:43:22 +0900

    Update c.js

--- c.js
+++ c.js
@@ -2,6 +2,8 @@
 var canvas = document.getElementById("canvas");
 var dc = canvas.getContext("2d");
 
+console.log( dc );
+
 for(i=0;i<10;i++){
   dc.fillStyle = "rgb(" + i*25 + ",0,0)";
   dc.fillRect(0+30*i,0, 30,30);
