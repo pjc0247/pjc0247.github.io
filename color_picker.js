60087283dfb6e43155489e4281466114a2443a16
Park June Chul <pjc0247@naver.com>
2014-05-11 19:46:22 +0900

    Update color_picker.js

--- color_picker.js
+++ color_picker.js
@@ -3,7 +3,11 @@ var dc = canvas.getContext("2d");
 
 toHSV = function(r,g,b){
   var h =
-    Math.acos( (0.5*(r-g) + (r-b)) / (Math.sqrt( Math.pow( (r-g), 2 ) + (r-b)(g-b)) ) );
+    Math.acos(
+      (0.5*(r-g) + (r-b))
+      /
+      (Math.sqrt( Math.pow( (r-g), 2 ) + (r-b)*(g-b)))
+    );
   var s = 1 - 3*( Math.min(r,g,b) / (r+g+b) );
   var v = (1/3) * (r+g+b);
   
