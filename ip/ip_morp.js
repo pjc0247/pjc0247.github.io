4f3d62dead9e77179833ce2f9c281b9e51bf7d8c
Park June Chul <pjc0247@naver.com>
2014-05-29 23:40:28 +0900

    Update ip_morp.js

--- ip/ip_morp.js
+++ ip/ip_morp.js
@@ -15,7 +15,7 @@ var ip_morp_erode = function(src, w,h){
       
       for(k=i;k<to_x;k++){
         for(l=j;l<to_y;l++){
-          var p = ip_get_rgb_at( src, i+k,j+l );
+          var p = ip_get_rgb_at( src, k,l );
           
           if( p[0] == 0 && p[1] == 0 && p[2] == 0 ){
             f = true;
@@ -25,7 +25,7 @@ var ip_morp_erode = function(src, w,h){
       }
       
       var px = ip_get_rgb_at(src,i,j);
-      if( px[0] )
+      if( f )
         ip_set_rgba_at( dst, u,v, 0,0,0,255 );
       else
         ip_set_rgba_at( dst, u,v, 255,255,255,255 );
