3245dffe01115d94ca12589c942bc73c72d9f18a
Park June Chul <pjc0247@naver.com>
2014-05-29 22:13:38 +0900

    Update ip_p.js

--- ip/ip_p.js
+++ ip/ip_p.js
@@ -30,5 +30,22 @@ var ip_lighten = function(dst, val){
   }
 }
 var ip_darken = function(dst, val){
-  ip_lighten( dst, -val );
+var roi = ip_get_roi( dst );
+  var ch_mask = [];
+
+  for(i=0;i<4;i++){
+    ch_mask[i] = ip_get_channel_mask( dst, i );
+  }
+  
+  for(i=roi[0];i<roi[2];i++){
+    for(j=roi[1];j<roi[3];j++){
+      var idx = (j * dst.width + i) * 4;
+      
+      for(k=0;k<IP_A;k++){
+        if( ip_is_in_range( dst.data[idx+k], ch_mask[k] ) ){
+          dst.data[idx+k] = Math.max( dst.data[idx+k]-val, 0 );
+        }
+      }
+    }
+  }
 }
