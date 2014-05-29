aab40cfe89defaaed156a3f543d3317ce6ef7fae
Park June Chul <pjc0247@naver.com>
2014-05-29 22:32:39 +0900

    Update ip_mask.js

--- ip/ip_mask.js
+++ ip/ip_mask.js
@@ -29,6 +29,7 @@ var ip_set_luminance_mask = function(dst, ch, min,max){
 
 /* getter */
 var ip_get_roi = function(dst){
+  console.log(dst);
   if( dst.ip_roi == undefined )
     return [0,0, dst.width,dst.height];
   else
