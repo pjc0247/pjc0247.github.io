39418f39a2d63a9a26e465a7ddf10b0e1a085694
Park June Chul <pjc0247@naver.com>
2014-05-29 22:00:16 +0900

    Update ip_p.js

--- ip/ip_p.js
+++ ip/ip_p.js
@@ -14,6 +14,7 @@ var ip_darken = function(dst, val){
   var ch_mask = [];
   
   console.log( roi );
+  console.log(dst);
   
   for(i=0;i<4;i++){
     ch_mask[i] = ip_get_channel_mask( dst, i );
@@ -24,7 +25,7 @@ var ip_darken = function(dst, val){
     for(j=roi.y;j<roi.height;j++){
       var idx = (j * dst.width + i) * 4;
       
-      for(k=IP_R;k<=IP_A;k++){
+      for(k=IP_R;k<=IP_B;k++){
         if( ip_is_in_range( dst.data[idx+k], ch_mask[k] ) ){
           console.log("Q");
           dst.data[idx+k] = Math.min( dst.data[idx+k]+val, 255 );
