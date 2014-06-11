7a89b88d902fcf1d09254acd923ad876fbc12b38
Park June Chul <pjc0247@naver.com>
2014-06-11 22:32:15 +0900

    Update minsoo.js

--- minsoo/minsoo.js
+++ minsoo/minsoo.js
@@ -1,12 +1,26 @@
+var count = document.getElementById("count");
 var canvas = document.getElementById("canvas");
 var dc = canvas.getContext("2d");
 
-var count = document.getElementById("count");
+var ms = new Array();
 
 var on_click = function(){
-  console.log("A" + count.value );
+  obj = {
+    x : 320, y : 240,
+    sx : Math.random() * 10, sy : Math.random() * 10,
+    alpha : 255
+  }
+  ms.push( obj );
 }
 
 setInterval( function(){
+  dc.clearRect(0,0,640,480);
+  
+  for( var i in ms ){
+    var obj = ms[i];
+    
+    dc.drawrText( "민수천재야", obj.x,obj.y )
+  }
+  
   dc.fill();
 }, 33 );
