3aeec9a1a2604a187303325ea59004b0f6048ee4
Park June Chul <pjc0247@naver.com>
2016-08-02 18:14:34 +0900

    Update c.js

--- c.js
+++ c.js
@@ -7,7 +7,7 @@ var downsampling_val = 10;
 var threshold_val = 100;
 
 var art = new Image();
-art.src = "album2.png";
+art.src = "album3.jpg";
 
 getLuminance = function(color){
   var rgb = color.split(',');
